b23105c365e0c2f46274dba4ab0c59e2e8344d70
Romain Kieffer <romain.kieffer57@gmail.com>
2021-10-08 15:10:20 +0200

    Staging fixups

--- testing_platform/settings.py
+++ testing_platform/settings.py
@@ -139,11 +139,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static_global/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static_global"),
-]
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    os.path.join(STATIC_ROOT, 'css'),
+    os.path.join(STATIC_ROOT, 'img'),
+    os.path.join(STATIC_ROOT, 'admin'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

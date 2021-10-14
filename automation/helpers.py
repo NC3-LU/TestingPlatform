2005da886cba2eee882d81b2369dd3ea338048be
Romain Kieffer <romain.kieffer57@gmail.com>
2021-10-14 21:00:08 +0200

    Automation app fixups, integrating domain models, user type and management features (ie add, view, delete tasks)

--- automation/helpers.py
+++ automation/helpers.py
@@ -4,11 +4,12 @@ from django_q.models import Schedule, Task
 def get_last_runs(tasks):
     last_runs = []
     for task in tasks:
+        target = task.target
         scheduled_task = Schedule.objects.get(name=task.schedule.name)
         all_runs = Task.objects.filter(group=scheduled_task.name)
         last_run = all_runs.last()
         for run in all_runs:
             if run.started >= last_run.started:
                 last_run = run
-        last_runs.append((scheduled_task, last_run))
+        last_runs.append((scheduled_task, target, last_run))
     return last_runs
--- automation/views.py
+++ automation/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
+from testing.models import UserDomain
 from .forms import PingAutomatedTestForm, HttpAutomatedTestForm
 from .models import PingAutomatedTest, HttpAutomatedTest
 from .helpers import get_last_runs
@@ -27,25 +28,33 @@ def schedule_ping(request):
     if request.method == 'POST':
         form = PingAutomatedTestForm(request.POST)
         if form.is_valid():
-            count = PingAutomatedTest.objects.filter(user=request.user).count()
-            if count >= 1 and not request.user.is_business:
-                messages.error(request, 'You already have one test set. Please upgrade account to BUSINESS '
-                                        'subscription if you wish to have more than one domain tested.')
-                return render(request, 'automation_request.html', {'form': form, 'title': 'ping test'})
             data = form.cleaned_data
             automation_request = PingAutomatedTest(
                 user=request.user,
-                host=data['host'],
+                target=data['target'],
                 frequency=data['frequency'],
                 time=data['time'],
                 weekday=data['weekday'],
                 monthly_test_date=data['monthly_test_date'],
             )
             automation_request.save()
-            return redirect('index')
+            return redirect('automation')
     else:
         form = PingAutomatedTestForm()
     return render(request, 'automation_request.html', {'form': form, 'title': 'ping test'})
+
+
+@login_required
+def remove_ping(request, domain):
+    ping_automated_test = PingAutomatedTest.objects.get(target__domain=domain)
+    scheduled_pings = Schedule.objects.filter(func='automation.tasks.ping').filter(args=f"'{domain}'")
+    ping_tasks = Task.objects.filter(func='automation.tasks.ping').filter(args=(domain,))
+    for item in scheduled_pings:
+        item.delete()
+    for item in ping_tasks:
+        item.delete()
+    ping_automated_test.delete()
+    return redirect('automation')
 
 
 @login_required
@@ -63,13 +72,25 @@ def schedule_http(request):
                 monthly_test_date=data['monthly_test_date'],
             )
             automation_request.save()
-            return redirect('index')
+            return redirect('automation')
     else:
         form = HttpAutomatedTestForm()
     return render(request, 'automation_request.html', {'form': form, 'title': 'http test'})
 
 
 @login_required
-def display_http_report(request, task_id):
-    last_run = Task.objects.filter(group=task_id).latest('started')
-    return render(request, 'web_report.html', last_run.result)
+def display_http_report(request, domain):
+    last_run = Task.objects.filter(func='automation.tasks.http').filter(args=(domain,)).latest('started')
+    return render(request, 'http_report.html', last_run.result)
+
+
+def remove_http_report(request, domain):
+    http_automated_test = HttpAutomatedTest.objects.get(target__domain=domain)
+    scheduled_http = Schedule.objects.filter(func='automation.tasks.http').filter(args=f"'{domain}'")
+    http_tasks = Task.objects.filter(func='automation.tasks.http').filter(args=(domain,))
+    for item in scheduled_http:
+        item.delete()
+    for item in http_tasks:
+        item.delete()
+    http_automated_test.delete()
+    return redirect('automation')

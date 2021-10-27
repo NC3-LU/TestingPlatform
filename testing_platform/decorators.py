9511cb4f223f0468ec2920df6574ffba6373148c
Romain Kieffer <romain.kieffer57@gmail.com>
2021-10-27 15:16:31 +0200

    Adding decorators

---
+++ testing_platform/decorators.py
@@ -0,0 +1,43 @@
+from functools import wraps
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+
+default_message = 'Unauthorised action.'
+
+
+def subscription_required(function):
+    @wraps(function)
+    def wrap(request, *args, **kwargs):
+        user_tier = request.user.tier_level
+        if user_tier in (1, 2):
+            return function(request, *args, **kwargs)
+        else:
+            messages.error(request, 'Unauthorized access')
+            return HttpResponseRedirect('/')
+    return wrap
+
+
+def pro_required(function):
+    @wraps(function)
+    def wrap(request, *args, **kwargs):
+        user_tier = request.user.tier_level
+        if user_tier == 'PRO':
+            return function(request, *args, **kwargs)
+        else:
+            messages.error(request, 'Unauthorized access')
+            return HttpResponseRedirect('/')
+
+    return wrap
+
+
+def business_required(function):
+    @wraps(function)
+    def wrap(request, *args, **kwargs):
+        user_tier = request.user.tier_level
+        if user_tier == 'BUSINESS':
+            return function(request, *args, **kwargs)
+        else:
+            messages.error(request, 'Unauthorized access')
+            return HttpResponseRedirect('/')
+
+    return wrap

821770870070b33478f07017e0438d728b8ad4e0
Romain Kieffer <romain.kieffer57@gmail.com>
2021-10-27 15:15:40 +0200

    Adding new ping to automated processes

--- automation/tasks.py
+++ automation/tasks.py
@@ -1,12 +1,21 @@
 import os
 import subprocess
 from testing.helpers import get_observatory_report
+import socket
+from ipwhois import IPWhois
 
 
 def ping(host):
-    response = subprocess.Popen(['ping', host, '-c', '2', '-W', '4'])
-    response.wait()
-    return response.poll()
+    # response = subprocess.Popen(['ping', host, '-c', '2', '-W', '4'])
+    # response.wait()
+    # return response.poll()
+    try:
+        ip = socket.gethostbyname(host)
+    except socket.gaierror:
+        return {'code': 1, 'result': "Can't fetch IP from host"}
+    whois = IPWhois(ip)
+    ping_result = whois.lookup_rdap(depth=1)
+    return {'code': 0, 'result': ping_result}
 
 
 def http(host):

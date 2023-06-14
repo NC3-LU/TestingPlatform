ec59840f7b8eb47573fa66ee8a99494d8df8af81
Romain Kieffer <romain.kieffer@nc3.lu>
2023-06-14 11:44:44 +0200

    fix: [settings] Fixed static files path format to BASE_DIR / dirname

--- testing_platform/settings.py
+++ testing_platform/settings.py
@@ -145,10 +145,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static_global"),
+    BASE_DIR / "static_global",
 ]
 
 # Default primary key field type

9c79d997769c1687437be9a0eccb4a721855945e
Cédric Bonhomme <cedric@cedricbonhomme.org>
2023-11-23 14:27:28 +0100

    chg: [API] Updated contrib/update.sh

--- contrib/update.sh
+++ contrib/update.sh
@@ -11,11 +11,32 @@ NC='\033[0m' # No Color
 set -e
 #set -x
 
+# Retrieve the update from the repository
 git pull origin main --tags
 npm ci
-. env/bin/activate
-# pip install -e .
-pip install
+
+
+# Search for a virtual environment folder in the project root
+VENV_NAME="venv"
+PROJECT_ROOT=$(pwd)
+
+# Find the virtual environment folder
+VENV_PATH=$(find "$PROJECT_ROOT" -type d -name "$VENV_NAME" 2>/dev/null | head -n 1)
+
+if [ -z "$VENV_PATH" ]; then
+    echo "Virtual environment not found in the project root."
+    exit 1
+fi
+
+# Activate the virtual environment
+source "$VENV_PATH/bin/activate"
+
+# source env/bin/activate
+
+
+
+pip install -e .
+# pip install
 python manage.py collectstatic --no-input
 python manage.py migrate
 # python manage.py compilemessages

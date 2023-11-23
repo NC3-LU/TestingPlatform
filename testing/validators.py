0e6328d5645a0105cedcdf90ab1d68c88d575141
Cédric Bonhomme <cedric@cedricbonhomme.org>
2023-11-23 11:08:26 +0100

    chg: [testing] Use the regex pattern from the API module.

--- testing/validators.py
+++ testing/validators.py
@@ -1,11 +1,4 @@
-import re
-
-pattern = re.compile(
-    r"^(([a-zA-Z]{1})|([a-zA-Z]{1}[a-zA-Z]{1})|"
-    r"([a-zA-Z]{1}[0-9]{1})|([0-9]{1}[a-zA-Z]{1})|"
-    r"([a-zA-Z0-9][-_.a-zA-Z0-9]{0,61}[a-zA-Z0-9]))\."
-    r"([a-zA-Z]{2,13}|[a-zA-Z0-9-]{2,30}.[a-zA-Z]{2,3})$"
-)
+from api.validators import pattern
 
 
 def full_domain_validator(value):

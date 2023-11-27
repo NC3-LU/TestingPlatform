381bbbd1454e776d1e162f7d99e9419d03d7d49b
Cédric Bonhomme <cedric@cedricbonhomme.org>
2023-11-27 14:27:38 +0100

    chg: [configuration] Cast MAX_UPLOAD_FILE_SIZE variable to int.

--- testing_platform/settings.py
+++ testing_platform/settings.py
@@ -46,7 +46,7 @@ EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "webmaster@localhost")
 
 # Maximum size of the file that can be sent to the API (/api/v1/InfraTesting/File/)
-MAX_UPLOAD_FILE_SIZE = 5000000
+MAX_UPLOAD_FILE_SIZE = int(os.environ.get("MAX_UPLOAD_FILE_SIZE", 5000000))
 
 # Application definition
 
